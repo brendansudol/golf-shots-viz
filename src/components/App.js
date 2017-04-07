951cedf852f75cb3a1d614c276c3e3bc15a731a2
brendan sudol <brendansudol@gmail.com>
2017-04-06 23:59:19 -0400

    add some example tourney data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+const img = `${process.env.PUBLIC_URL}/data/tourneys/1/course/holes/03_full.jpg`
+
 class App extends Component {
   render() {
     return (
       <div className='p3'>
         <div className='h1'>🏌</div>
+        <img src={img} alt='hole' />
       </div>
     )
   }
