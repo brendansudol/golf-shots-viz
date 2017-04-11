05ce59f72948c0c35ba371dd2c3474c31af6d7ee
brendan sudol <brendansudol@gmail.com>
2017-04-10 23:24:58 -0400

    try out new react router

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => (
+  <div className='p2'>
+    <h2>stay tuned...</h2>
+  </div>
+)
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { HashRouter, Route } from 'react-router-dom'
 
-import Hole from './Hole'
+import About from './About'
+import Main from './Main'
 
-const players = [
-  {"id":"27649","name":"Brandt Snedeker"},
-  {"id":"40058","name":"Zac Blair"},
-  {"id":"37455","name":"Si Woo Kim"},
-  {"id":"20691","name":"Greg Owen"},
-  {"id":"29478","name":"Kevin Kisner"},
-  {"id":"29461","name":"Jamie Lovemark"},
-  {"id":"27436","name":"Graham DeLaet"},
-  {"id":"24024","name":"Zach Johnson"},
-  {"id":"08075","name":"Jerry Kelly"},
-  {"id":"34264","name":"Hudson Swafford"}
-]
-
-class App extends Component {
-  state = { data: null, holeNum: 0, playerId: '27649', view: 'full' }
-
-  componentDidMount() {
-    fetch(`${process.env.PUBLIC_URL}/data/tourneys/1/summary.json`)
-      .then(response => response.json())
-      .then(data => this.setState({ data }))
-  }
-
-  handleChange = e => {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
-  }
-
-  render() {
-    const { data, holeNum, playerId, view } = this.state
-
-    return (
-      <div className='p2'>
-        <div className='h1'>🏌</div>
-        <select
-          name='holeNum'
-          onChange={this.handleChange}
-          value={holeNum}
-        >
-          {[...Array(18)].map((_, i) => (
-            <option key={i} value={i}>{i + 1}</option>
-          ))}
-        </select>
-        <select
-          name='playerId'
-          onChange={this.handleChange}
-          value={playerId}
-        >
-          {players.map(p => (
-            <option key={p.id} value={p.id}>{p.name}</option>
-          ))}
-        </select>
-        <select
-          name='view'
-          onChange={this.handleChange}
-          value={view}
-        >
-          <option>full</option>
-          <option>green</option>
-        </select>
-        {!data
-          ? <div>Loading...</div>
-          : <Hole {...this.state} />
-        }
-      </div>
-    )
-  }
-}
+const App = () => (
+  <HashRouter>
+    <div>
+      <Route exact path="/" component={Main} />
+      <Route path="/about" component={About} />
+    </div>
+  </HashRouter>
+)
 
 export default App
